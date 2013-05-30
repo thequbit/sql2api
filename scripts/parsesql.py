b7c470500a40bc5b3997093583b7a288f9bf998f
thequbit <thequbit@gmail.com>
2013-05-30 13:38:24 -0400

    fixed some errors with ()'s being split() on wrong

--- scripts/parsesql.py
+++ scripts/parsesql.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import os
 
 def parsesql(sql):
@@ -15,6 +16,12 @@ def parsesql(sql):
     print "[INFO] Parsing SQL ..."
 
     statements = sql.split(";")
+
+#    for statement in statements:
+#        print "{0}\n".format(statement)
+
+
+    #return ("",[])
 
     #print statements
 
@@ -38,18 +45,22 @@ def parsesql(sql):
             if parts[1].lower() == "table" and len(parts) >= 3:
 
                 tname = parts[2].split("(")[0].strip()
-
-                print "[INFO] Found Table: {0}".format(tname)
-
+                meat = statement[statement.index("(")+1:]
+#                print "\tMeat: {0}".format(meat)
                 columns = []
 
-                cols = statement.split("(")[1].split(",")
+                cols = meat.split(",")
+
                 for col in cols:
-                    col = col.replace(")","")
                     col = col.strip()
+                    if len(col) < 5:
+                        continue;
+#                    print "\tColumn Raw: {0}".format(col)
 
                     cname = col.split(" ")[0]
                     ctype = col.split(" ")[1]
+
+#                    print "\tColumn: `{0}` of type `{1}`".format(cname,ctype)
 
                     if "primary key" in col.lower():
                         cisprimarykey = True
@@ -59,6 +70,8 @@ def parsesql(sql):
                     if cname.lower() != "foreign":
                         #print "\t{0} : {1}".format(cname,ctype,cisprimarykey)
                         columns.append((cname,ctype,cisprimarykey))
+
+                print "[INFO] Found Table: `{0}` with {1} columns".format(tname,len(columns))
 
                 tables.append((tname,columns))
 
