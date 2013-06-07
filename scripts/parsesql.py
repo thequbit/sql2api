0ee482548851bc832e080e8c3d264ea0791c5932
thequbit <thequbit@gmail.com>
2013-06-07 12:51:41 -0400

    added index as a keyword to ignore

--- scripts/parsesql.py
+++ scripts/parsesql.py
@@ -36,39 +36,30 @@ def parsesql(sql):
         if parts[0].lower() == "create" and len(parts) >= 2:
 
             if parts[1].lower() == "database" and len(parts) >= 3:
-
                 dbname = parts[2].strip().replace(";","")
-
                 print "[INFO] Database Name: {0}".format(dbname)
 
 
             if parts[1].lower() == "table" and len(parts) >= 3:
-
                 tname = parts[2].split("(")[0].strip()
                 meat = statement[statement.index("(")+1:]
-#                print "\tMeat: {0}".format(meat)
+
                 columns = []
-
                 cols = meat.split(",")
-
                 for col in cols:
                     col = col.strip()
                     if len(col) < 5:
                         continue;
-#                    print "\tColumn Raw: {0}".format(col)
 
                     cname = col.split(" ")[0]
                     ctype = col.split(" ")[1]
-
-#                    print "\tColumn: `{0}` of type `{1}`".format(cname,ctype)
 
                     if "primary key" in col.lower():
                         cisprimarykey = True
                     else:
                         cisprimarykey = False
 
-                    if cname.lower() != "foreign":
-                        #print "\t{0} : {1}".format(cname,ctype,cisprimarykey)
+                    if cname.lower() != "foreign" and cname.lower() != "index":
                         columns.append((cname,ctype,cisprimarykey))
 
                 print "[INFO] Found Table: `{0}` with {1} columns".format(tname,len(columns))
